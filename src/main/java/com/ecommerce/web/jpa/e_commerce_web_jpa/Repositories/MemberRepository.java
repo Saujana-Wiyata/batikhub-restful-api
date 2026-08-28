0f66737f33f5aac197469b314c1fdd47585df7fc
Diamond Patrick <diamondpaatrick@gmail.com>
2026-08-28 15:07:13 +0700

    modified return data type to Optional<> for finByEmailAndPassword method

--- src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Repositories/MemberRepository.java
+++ src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Repositories/MemberRepository.java
@@ -1,11 +1,13 @@
 package com.ecommerce.web.jpa.e_commerce_web_jpa.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ecommerce.web.jpa.e_commerce_web_jpa.entities.Member;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
 
-    Member findByEmailAndPassword(String email, String password);
+    Optional<Member> findByEmailAndPassword(String email, String password);
 
 }
