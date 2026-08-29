b54e2b0b0c9c3607231a567c18c340256084b86d
Diamond Patrick <diamondpaatrick@gmail.com>
2026-08-29 18:02:22 +0700

    change exception package to the correct one.

--- src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/exception/GlobalErrorHandling.java
+++ src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/exception/GlobalErrorHandling.java
@@ -1,36 +1,38 @@
 package com.ecommerce.web.jpa.e_commerce_web_jpa.exception;
 
-import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.ecommerce.web.jpa.e_commerce_web_jpa.model.WebResponse;
+
+import jakarta.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 public class GlobalErrorHandling {
 
-    @ExceptionHandler(exception = ConstraintViolationException.class)
-    public ResponseEntity<WebResponse<String>> constraintViolationHandler(
-            ConstraintViolationException exception) {
+        @ExceptionHandler(exception = ConstraintViolationException.class)
+        public ResponseEntity<WebResponse<String>> constraintViolationHandler(
+                        ConstraintViolationException exception) {
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                WebResponse.<String>builder()
-                        .code(HttpStatus.BAD_REQUEST.value())
-                        .error(exception.getMessage())
-                        .build());
-    }
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                                WebResponse.<String>builder()
+                                                .code(HttpStatus.BAD_REQUEST.value())
+                                                .error(exception.getMessage())
+                                                .build());
+        }
 
-    @ExceptionHandler(exception = Exception.class)
-    public ResponseEntity<WebResponse<String>> exceptionHandler(
-            Exception exception) {
+        @ExceptionHandler(exception = ResponseStatusException.class)
+        public ResponseEntity<WebResponse<String>> exceptionHandler(
+                        ResponseStatusException exception) {
 
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(WebResponse.<String>builder()
-                        .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                        .error(exception.getMessage())
-                        .build());
-    }
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                .body(WebResponse.<String>builder()
+                                                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                                                .error(exception.getMessage())
+                                                .build());
+        }
 
 }
