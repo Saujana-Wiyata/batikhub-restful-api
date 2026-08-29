ede61150328d6bce60410aca997f0c056143ff8d
Diamond Patrick <diamondpaatrick@gmail.com>
2026-08-29 18:05:59 +0700

    relocated dto class

--- src/test/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Service/MemberServiceTest.java
+++ src/test/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Service/MemberServiceTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.ecommerce.web.jpa.e_commerce_web_jpa.dto.member.AlamatInsert;
-import com.ecommerce.web.jpa.e_commerce_web_jpa.dto.member.MemberRequest;
-import com.ecommerce.web.jpa.e_commerce_web_jpa.dto.member.MemberResponse;
-import com.ecommerce.web.jpa.e_commerce_web_jpa.dto.member.MemberUpdateRequest;
 import com.ecommerce.web.jpa.e_commerce_web_jpa.entities.embed.Alamat;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.model.member.AlamatInsert;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.model.member.MemberRequest;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.model.member.MemberResponse;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.model.member.MemberUpdateRequest;
 import com.ecommerce.web.jpa.e_commerce_web_jpa.service.member.MemberService;
 
 import jakarta.validation.ConstraintViolationException;
@@ -40,7 +40,7 @@ public class MemberServiceTest {
         member.setName("Syakir Jamil");
         member.setEmail("syakir@gmail.com");
         member.setPassword("syakir");
-        member.setAlamatDto(new AlamatInsert(" ",
+        member.setAlamatDto(new AlamatInsert("",
                 "Surakarta", "Jawa Tengah"));
 
         Assertions.assertThrows(ConstraintViolationException.class, () -> {
