5a246d219410fd8903720994617222590ff37ef6
Diamond Patrick <diamondpaatrick@gmail.com>
2026-08-29 18:04:58 +0700

    Test controller insert

--- src/test/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Controller/MemberControllerTest.java
+++ src/test/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Controller/MemberControllerTest.java
@@ -1,13 +1,25 @@
 package com.ecommerce.web.jpa.e_commerce_web_jpa.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.model.WebResponse;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.model.member.AlamatInsert;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.model.member.MemberRequest;
+
+import tools.jackson.core.JacksonException;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,45 +28,62 @@ public class MemberControllerTest {
         @Autowired
         private MockMvc mockMvc;
 
+        @Autowired
+        private ObjectMapper objectMapper;
+
         @Test
-        void testPromoPage() throws Exception {
+        void testInsertSuccess() throws JacksonException, Exception {
+
+                MemberRequest memberRequest = new MemberRequest();
+                memberRequest.setName("Syakir");
+                memberRequest.setEmail("syakirlongoi@gmail.com");
+                memberRequest.setPassword("syakir123");
+                memberRequest.setAlamatDto(new AlamatInsert("jalan buntu",
+                                "Johor Bahru", "johor"));
+
                 mockMvc.perform(
-                                get("/promo")).andExpect(status().isOk());
+                                post("/api/v1/member")
+                                                .accept(MediaType.APPLICATION_JSON_VALUE)
+                                                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                                                .content(objectMapper.writeValueAsString(memberRequest)))
+                                .andExpect(status().isOk())
+                                .andDo(result -> {
+                                        WebResponse<String> value = objectMapper.readValue(
+                                                        result.getResponse().getContentAsString(),
+                                                        new TypeReference<WebResponse<String>>() {
+                                                        });
+
+                                        assertEquals(200, value.getCode());
+                                        assertEquals("OK", value.getData());
+                                });
         }
 
         @Test
-        void testCariPage() throws Exception {
-                mockMvc.perform(get("/findproduct")).andExpect(status().isOk());
-        }
+        void testInsertFail() throws JacksonException, Exception {
 
-        @Test
-        void testCariProduk() throws Exception {
-                mockMvc.perform(
-                                post("/findbyname")
-                                                .param("produkName", "Kemeja"))
-                                .andExpectAll(
-                                                status().isOk(),
-                                                view().name("visitor/cariPage"),
-                                                model().attribute("totalProduk", 1));
-        }
+                MemberRequest memberRequest = new MemberRequest();
+                memberRequest.setName("Syakir");
+                memberRequest.setEmail("syakirlongoi@gmail.com");
+                memberRequest.setPassword("syakir123");
+                memberRequest.setAlamatDto(new AlamatInsert(" ",
+                                "Johor Bahru", "johor"));
 
-        @Test
-        void testCariProdukZeroResult() throws Exception {
                 mockMvc.perform(
-                                post("/findbyname")
-                                                .param("produkName", "jkhxsna"))
-                                .andExpectAll(
-                                                status().isOk(),
-                                                view().name("visitor/cariPage"),
-                                                model().attribute("totalProduk", 0));
-        }
+                                post("/api/v1/member")
+                                                .accept(MediaType.APPLICATION_JSON_VALUE)
+                                                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                                                .content(objectMapper.writeValueAsString(memberRequest)))
+                                .andExpect(status().isBadRequest())
+                                .andDo(result -> {
+                                        WebResponse<String> value = objectMapper.readValue(
+                                                        result.getResponse().getContentAsString(),
+                                                        new TypeReference<WebResponse<String>>() {
+                                                        });
 
-        @Test
-        void testGetBuyPage() throws Exception {
-                mockMvc.perform(
-                                get("/buyproduct")
-                                                .param("idProduk", "A02"))
-                                .andExpect(
-                                                status().isOk());
+                                        assertNotEquals(200, value.getCode());
+                                        assertNotNull(value.getError());
+                                        System.out.println(value.getError());
+                                });
+
         }
 }
