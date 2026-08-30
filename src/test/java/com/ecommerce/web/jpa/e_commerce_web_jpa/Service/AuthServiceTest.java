34a6b09cf1514d24b26f26ca3addb7e5539ff54e
Diamond Patrick <diamondpaatrick@gmail.com>
2026-08-30 15:41:10 +0700

    Create Auth Testing class. test for loginByEmailAndPassword

---
+++ src/test/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Service/AuthServiceTest.java
@@ -0,0 +1,49 @@
+package com.ecommerce.web.jpa.e_commerce_web_jpa.service;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.ecommerce.web.jpa.e_commerce_web_jpa.model.auth.AuthRequest;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.model.auth.AuthResponse;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.service.auth.AuthService;
+
+import jakarta.validation.ConstraintViolationException;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthServiceTest {
+
+    @Autowired
+    private AuthService authService;
+
+    @Test
+    void testMemberLogin() {
+        AuthResponse member = authService.loginByEmailAndPassword(
+                new AuthRequest("syakirlongoi@gmail.com", "syakir123"));
+
+        assertNotNull(member.getToken());
+        assertNotNull(member.getTokenExpired());
+    }
+
+    @Test
+    void testMemberLoginBlank() {
+        assertThrows(ConstraintViolationException.class, () -> {
+            authService.loginByEmailAndPassword(
+                    new AuthRequest("syakirlongoi@gmail.com", ""));
+        });
+    }
+
+    @Test
+    void testMemberLoginNotMatch() {
+        assertThrows(ResponseStatusException.class, () -> {
+            authService.loginByEmailAndPassword(
+                    new AuthRequest("syakirlongoi@gmail.com", "123rahasia"));
+        });
+    }
+}
