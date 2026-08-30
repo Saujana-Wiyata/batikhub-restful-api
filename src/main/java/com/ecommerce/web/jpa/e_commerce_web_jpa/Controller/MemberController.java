f15f2ccefac603680e21800d50005ea311e3fb2f
Diamond Patrick <diamondpaatrick@gmail.com>
2026-08-30 16:50:53 +0700

    Create and implment logic for update method

--- src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Controller/MemberController.java
+++ src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Controller/MemberController.java
@@ -4,14 +4,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.web.jpa.e_commerce_web_jpa.model.WebResponse;
 import com.ecommerce.web.jpa.e_commerce_web_jpa.model.member.MemberRequest;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.model.member.MemberResponse;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.model.member.MemberUpdateRequest;
 import com.ecommerce.web.jpa.e_commerce_web_jpa.service.member.MemberService;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,4 +33,15 @@ public class MemberController {
                 .build();
     }
 
+    @PatchMapping(path = "/api/v1/member/current", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponse<MemberResponse> updateMemberData(
+            @RequestBody MemberUpdateRequest memberUpdateRequest,
+            @RequestHeader String token) {
+
+        MemberResponse update = memberService.update(token, memberUpdateRequest);
+        return WebResponse.<MemberResponse>builder()
+                .code(HttpStatus.OK.value())
+                .data(update)
+                .build();
+    }
 }
