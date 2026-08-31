61bf64aa2c5a7d67d2543f921d512b8510e6c241
Diamond Patrick <diamondpaatrick@gmail.com>
2026-08-31 21:33:30 +0700

    Craete findByToken method

--- src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Repositories/StaffRepository.java
+++ src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Repositories/StaffRepository.java
@@ -1,10 +1,14 @@
 package com.ecommerce.web.jpa.e_commerce_web_jpa.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ecommerce.web.jpa.e_commerce_web_jpa.entities.Staff;
 
 public interface StaffRepository extends JpaRepository<Staff, String> {
 
-    Staff findByEmailAndPassword(String email, String password);
+    Optional<Staff> findByEmailAndPassword(String email, String password);
+
+    Optional<Staff> findByToken(String token);
 }
