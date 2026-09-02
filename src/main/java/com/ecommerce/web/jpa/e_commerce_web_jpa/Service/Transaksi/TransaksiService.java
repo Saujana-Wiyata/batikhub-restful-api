b89977f0f49c6d9f19552c6c1fd9f6a5863f6fc6
Diamond Patrick <diamondpaatrick@gmail.com>
2026-09-03 01:22:49 +0700

    Create service class for Transaksi and implement logic for all method

--- src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Service/Transaksi/TransaksiService.java
+++ src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Service/Transaksi/TransaksiService.java
@@ -1,22 +1,19 @@
 package com.ecommerce.web.jpa.e_commerce_web_jpa.service.transaksi;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
 
-import com.ecommerce.web.jpa.e_commerce_web_jpa.dto.transaksi.TransaksiInsertDTO;
-import com.ecommerce.web.jpa.e_commerce_web_jpa.entities.Transaksi;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.model.transaksi.TransaksiInsertRequest;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.model.transaksi.TransaksiResponse;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 
 public interface TransaksiService {
 
-    void insert(@Valid TransaksiInsertDTO transaksi);
+    void insert(@Valid TransaksiInsertRequest transaksi);
 
     void delete(@Positive int idTransaksi);
 
-    List<Transaksi> findAll();
-
-    // TODO : delete this method because it dont use it in controller
-    Transaksi findById(@Positive int idTransaksi);
+    Page<TransaksiResponse> findAll();
 
 }
--- src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Service/Transaksi/TransaksiServiceImpl.java
+++ src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Service/Transaksi/TransaksiServiceImpl.java
@@ -1,17 +1,25 @@
 package com.ecommerce.web.jpa.e_commerce_web_jpa.service.transaksi;
 
-import java.util.List;
+import java.time.LocalDate;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.server.ResponseStatusException;
 
-import com.ecommerce.web.jpa.e_commerce_web_jpa.dto.transaksi.TransaksiInsertDTO;
 import com.ecommerce.web.jpa.e_commerce_web_jpa.entities.Member;
 import com.ecommerce.web.jpa.e_commerce_web_jpa.entities.Produk;
 import com.ecommerce.web.jpa.e_commerce_web_jpa.entities.Transaksi;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.model.transaksi.TransaksiInsertRequest;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.model.transaksi.TransaksiResponse;
 import com.ecommerce.web.jpa.e_commerce_web_jpa.service.omset.OmsetService;
 import com.ecommerce.web.jpa.e_commerce_web_jpa.service.produk.ProdukService;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.repositories.MemberRepository;
 import com.ecommerce.web.jpa.e_commerce_web_jpa.repositories.TransaksiRepository;
+
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
@@ -21,30 +29,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TransaksiServiceImpl implements TransaksiService {
 
+    private final MemberRepository memberRepository;
+
     private final TransaksiRepository transaksiRepository;
 
     private final ProdukService produkService;
 
     private final OmsetService omsetService;
-
-    @Override
-    public void insert(@Valid TransaksiInsertDTO transaksi) {
-
-        Member member = new Member();
-        member.setId(transaksi.getIdMember());
-
-        Produk produk = new Produk();
-        produk.setId(transaksi.getIdProduk());
-
-        Transaksi transaksiEntity = new Transaksi();
-        transaksiEntity.setTotalPembelian(transaksi.getTotalPembelian());
-        transaksiEntity.setPurchaseDate(transaksi.getPurchaseDate());
-        transaksiEntity.setArrivalDate(transaksi.getArrivalDate());
-        transaksiEntity.setIdMember(member);
-        transaksiEntity.setIdProduk(produk);
-
-        transaksiRepository.save(transaksiEntity);
-    }
 
     private void reduceStack(Transaksi transaksi) {
         produkService.kurangiStock(transaksi.getTotalPembelian(), transaksi
@@ -57,35 +48,62 @@ public class TransaksiServiceImpl implements TransaksiService {
     }
 
     private void breakRelation(Transaksi transaksi) {
-        transaksi.getIdMember().setListTransaksi(null);
+        transaksi.getTokenMember().setListTransaksi(null);
         transaksi.getIdProduk().setListTransaksi(null);
 
-        transaksi.setIdMember(null);
+        transaksi.setTokenMember(null);
         transaksi.setIdProduk(null);
     }
 
     @Override
+    @Transactional
+    public void insert(@Valid TransaksiInsertRequest transaksi) {
+
+        Member member = new Member();
+        Member tokenMember = memberRepository.findByToken(transaksi.getTokenMember())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "token not detected"));
+        member.setId(tokenMember.getId());
+
+        Produk produk = new Produk();
+        produk.setId(transaksi.getIdProduk());
+
+        Transaksi transaksiEntity = new Transaksi();
+        transaksiEntity.setTotalPembelian(transaksi.getTotalPembelian());
+        transaksiEntity.setPurchaseDate(LocalDate.now());
+        transaksiEntity.setArrivalDate(LocalDate.now().plusDays(7));
+        transaksiEntity.setTokenMember(member);
+        transaksiEntity.setIdProduk(produk);
+
+        transaksiRepository.save(transaksiEntity);
+    }
+
+    @Override
+    @Transactional
     public void delete(@Positive int idTransaksi) {
         Transaksi transaksi = transaksiRepository.findById(idTransaksi)
-                .orElse(null);
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "fail to delete transaksi"));
 
         reduceStack(transaksi);
         addJumlahPenjualan(transaksi);
-
         breakRelation(transaksi);
 
         transaksiRepository.delete(transaksi);
     }
 
     @Override
-    public List<Transaksi> findAll() {
-        return transaksiRepository.findAll();
-    }
+    @Transactional(readOnly = true)
+    public Page<TransaksiResponse> findAll() {
+        PageRequest paging = PageRequest.of(0, 5);
 
-    // TODO : delete this method because it dont use it in controller
-    @Override
-    public Transaksi findById(@Positive int idTransaksi) {
-        return transaksiRepository.findById(idTransaksi).orElse(null);
+        return transaksiRepository.findAll(paging)
+                .map(transaksi -> TransaksiResponse.builder()
+                        .id(transaksi.getId())
+                        .totalPembelian(transaksi.getTotalPembelian())
+                        .purchaseDate(transaksi.getPurchaseDate())
+                        .arrivalDate(transaksi.getArrivalDate())
+                        .idMember(transaksi.getTokenMember().getId())
+                        .idProduk(transaksi.getIdProduk().getId())
+                        .build());
     }
 
 }
